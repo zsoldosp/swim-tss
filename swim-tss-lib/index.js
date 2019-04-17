12a0b840eb1a84a0c0b0112686ec5126eff7d261
Peter Zsoldos <peter.zsoldos@gmail.com>
2019-04-17 15:45:04 +0200

    refactoring: change to input -> calc -> returned output style of coding

--- swim-tss-lib/index.js
+++ swim-tss-lib/index.js
@@ -1,3 +1,21 @@
+function calculateIntervalStats({reps, distancePerRep, paceInSeconds, restPerRepInSeconds, ftpPaceInSeconds}) {
+  let res = {};
+  res.totalDistance = distancePerRep * reps;
+  res.intensity = ftpPaceInSeconds / paceInSeconds;
+
+  res.perRepSwimTimeInSeconds = (distancePerRep / 100) * paceInSeconds;
+  res.totalSwimTimeInSeconds = res.perRepSwimTimeInSeconds * reps;
+  res.totalRestTimeInSeconds = reps * restPerRepInSeconds; // TODO: shouldn't be reps - 1?
+
+  res.totalSwimTimeInHours = res.totalSwimTimeInSeconds / 60 / 60;
+  res.sTSSBeforeRounding = Math.pow(res.intensity, 3)*100 * res.totalSwimTimeInHours;
+  res.sTSS = Math.round(res.sTSSBeforeRounding); // TODO: remove once confirmed with AC?
+  res.totalTimeInSeconds = res.totalSwimTimeInSeconds + res.totalRestTimeInSeconds;
+  res.totalTimeInMinutesRounded = Math.round(res.totalSwimTimeInSeconds/60) + Math.round(res.totalRestTimeInSeconds / 60); // TODO: don't round as it causes problems in aggregation (total time, thus in IF calculation)
+  // e.g.: FTP 90s / 100, 1x200 @ 100s / 100 pace with 10 secs of rest, IF rounded 0.93, not rounded 0.88
+  return res;
+}
+
 class Workout {
   constructor() {
     this.intervals = [];
@@ -39,19 +57,12 @@ class Interval {
   }
 
   calculateStats(ftpPaceInSeconds) {
-    this.totalDistance = this.distancePerRep * this.reps;
-    this.intensity = ftpPaceInSeconds / this.paceInSeconds;
-
-    this.perRepSwimTimeInSeconds = (this.distancePerRep / 100) * this.paceInSeconds;
-    this.totalSwimTimeInSeconds = this.perRepSwimTimeInSeconds * this.reps;
-    this.totalRestTimeInSeconds = this.reps * this.restPerRepInSeconds; // TODO: shouldn't be reps - 1?
-
-    this.totalSwimTimeInHours = this.totalSwimTimeInSeconds / 60 / 60;
-    this.sTSSBeforeRounding = Math.pow(this.intensity, 3)*100 * this.totalSwimTimeInHours;
-    this.sTSS = Math.round(this.sTSSBeforeRounding); // TODO: remove once confirmed with AC?
-    this.totalTimeInSeconds = this.totalSwimTimeInSeconds + this.totalRestTimeInSeconds;
-    this.totalTimeInMinutesRounded = Math.round(this.totalSwimTimeInSeconds/60) + Math.round(this.totalRestTimeInSeconds / 60); // TODO: don't round as it causes problems in aggregation (total time, thus in IF calculation)
-    // e.g.: FTP 90s / 100, 1x200 @ 100s / 100 pace with 10 secs of rest, IF rounded 0.93, not rounded 0.88
+    this.ftpPaceInSeconds = ftpPaceInSeconds;
+    let stats = calculateIntervalStats(this);
+    let self = this;
+    Object.getOwnPropertyNames(stats).forEach(function(propertyName) {
+      self[propertyName] = stats[propertyName];
+    });
   }
 }
 
